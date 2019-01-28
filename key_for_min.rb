04008e2ba1fabc9d7edadcef585224899136a4bb
Mark Kanko <mkanko66@gmail.com>
2019-01-28 23:39:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,11 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-  name_hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v 
-      lowest_key = k
+    name_hash.each do |k, v|
+      if lowest_value == nil || v < lowest_value
+        lowest_value = v 
+        lowest_key = k
+      end
     end
-
-
-  end
   lowest_key
 end  
